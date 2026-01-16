ecdcdc68667409586fa46f236523513ca729bcba
murat-can-tutar <tutarmurat66@gmail.com>
2026-01-16 15:16:04 +0300

    Kod

--- nodemcu-ota-relay.ino
+++ nodemcu-ota-relay.ino
@@ -1,33 +1,58 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
+#include <ESP8266HTTPUpdateServer.h> // OTA için gerekli kütüphane
 
-#define RELAY_PIN D1
+// --- AYARLAR ---
+const char* ssid = "MCT LED";      
+const char* password = "192513";   
+
+// Sabit IP (ESP32'deki nodeMcuIP ile aynı olmalı)
+IPAddress local_IP(192, 168, 4, 50);
+IPAddress gateway(192, 168, 4, 1);
+IPAddress subnet(255, 255, 255, 0);
+
+#define RELAY_PIN D1 
 
 ESP8266WebServer server(80);
+ESP8266HTTPUpdateServer httpUpdater; // OTA Sunucusu
+
+void handleAC() {
+  digitalWrite(RELAY_PIN, HIGH); 
+  server.send(200, "text/plain", "Relay ON");
+  Serial.println("Komut: ROLE ACIK");
+}
+
+void handleKapat() {
+  digitalWrite(RELAY_PIN, LOW); 
+  server.send(200, "text/plain", "Relay OFF");
+  Serial.println("Komut: ROLE KAPALI");
+}
 
 void setup() {
+  Serial.begin(115200);
   pinMode(RELAY_PIN, OUTPUT);
-  digitalWrite(RELAY_PIN, LOW);
+  digitalWrite(RELAY_PIN, LOW); 
 
-WiFi.mode(WIFI_STA);
-IPAddress ip(192,168,4,2);
-IPAddress gateway(192,168,4,1);
-IPAddress subnet(255,255,255,0);
-WiFi.config(ip, gateway, subnet);
-WiFi.begin("MCT LED", "192513356");
+  if (!WiFi.config(local_IP, gateway, subnet)) {
+    Serial.println("Sabit IP Hatası!");
+  }
 
-  server.on("/relay", HTTP_GET, []() {
-    if (server.hasArg("state")) {
-      String s = server.arg("state");
-      digitalWrite(RELAY_PIN, s == "1" ? HIGH : LOW);
-      server.send(200, "text/plain", "OK");
-    } else {
-      server.send(400, "text/plain", "ERR");
-    }
-  });
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  
+  Serial.println("\nBaglandi!");
 
-  server.on("/status", HTTP_GET, []() {
-    server.send(200, "text/plain", "OK");
+  // --- OTA AYARI ---
+  // Tarayıcıdan 192.168.4.50/update adresine girerek güncelleme yapabilirsin
+  httpUpdater.setup(&server); 
+
+  server.on("/ac", handleAC);
+  server.on("/kapat", handleKapat);
+  server.on("/", []() {
+    server.send(200, "text/html", "<h3>NodeMCU Isitici Sistemi</h3><p>OTA icin: <a href='/update'>/update</a></p>");
   });
 
   server.begin();
