b0419fe53d689c5436c3c7a3683d64b3171bfdd4
murat-can-tutar <tutarmurat66@gmail.com>
2026-01-16 11:48:09 +0300

    Kod

--- nodemcu-ota-relay.ino
+++ nodemcu-ota-relay.ino
@@ -1,46 +1,35 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
-#include <ESP8266HTTPUpdateServer.h>
 
 #define RELAY_PIN D1
 
 ESP8266WebServer server(80);
-ESP8266HTTPUpdateServer httpUpdater;
-
-bool relayState = false;
-
-void handleRoot() {
-  String page = "<html><body style='text-align:center;font-family:Arial;'>";
-  page += "<h2>Röle Kontrol</h2>";
-  page += "<p>Durum: <b>";
-  page += relayState ? "ACIK" : "KAPALI";
-  page += "</b></p>";
-  page += "<form action='/toggle' method='POST'>";
-  page += "<button style='width:200px;height:60px;font-size:20px;'>AC / KAPA</button>";
-  page += "</form>";
-  page += "<br><a href='/update'>OTA Guncelle</a>";
-  page += "</body></html>";
-  server.send(200, "text/html", page);
-}
-
-void handleToggle() {
-  relayState = !relayState;
-  digitalWrite(RELAY_PIN, relayState ? HIGH : LOW);
-  server.sendHeader("Location", "/");
-  server.send(303);
-}
 
 void setup() {
   pinMode(RELAY_PIN, OUTPUT);
   digitalWrite(RELAY_PIN, LOW);
 
-  WiFi.mode(WIFI_AP);
-  WiFi.softAP("NodeMCU-OTA", "12345678");
+WiFi.mode(WIFI_STA);
+IPAddress ip(192,168,4,2);
+IPAddress gateway(192,168,4,1);
+IPAddress subnet(255,255,255,0);
+WiFi.config(ip, gateway, subnet);
+WiFi.begin("MCT LED", "192513356");
 
-  server.on("/", handleRoot);
-  server.on("/toggle", HTTP_POST, handleToggle);
+  server.on("/relay", HTTP_GET, []() {
+    if (server.hasArg("state")) {
+      String s = server.arg("state");
+      digitalWrite(RELAY_PIN, s == "1" ? HIGH : LOW);
+      server.send(200, "text/plain", "OK");
+    } else {
+      server.send(400, "text/plain", "ERR");
+    }
+  });
 
-  httpUpdater.setup(&server);
+  server.on("/status", HTTP_GET, []() {
+    server.send(200, "text/plain", "OK");
+  });
+
   server.begin();
 }
 
